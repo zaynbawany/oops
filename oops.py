ff48178232c9f6f09aaee59bcc8c449e14e628db
zbawany <zayn.bawany@gmail.com>
2024-09-28 17:09:58 +0500

    commit

---
+++ oops.py
@@ -0,0 +1,11 @@
+class student:
+    grade=90
+    print(grade)
+ob=student()  
+class vehicle:
+    def __init__(self,maxspeed,mileage):
+         self.maxspeed=maxspeed
+         self.mileage=mileage
+ob2=vehicle(250,100)
+print(ob2.maxspeed)
+print(ob2.mileage)
